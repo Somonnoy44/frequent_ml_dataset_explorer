a97d434acd0c0b366a9c985e877d5bd95afc8d0a
Somonnoy44 <51916219+Somonnoy44@users.noreply.github.com>
2022-12-20 12:11:57 -0500

    Initial Commit
    Adding files app.py and README.md

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+import streamlit as st
+
+# EDA Libraries
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('Agg')
+
+import seaborn as sns
+
+def main():
+	""" Frequent ML Dataset Explorer """
+	st.title("Frequent ML Dataset Explorer")
+	st.subheader("Dataset Explorer using Streamlit")
+
+
+
+if __name__ == '__main__':
+	main()
